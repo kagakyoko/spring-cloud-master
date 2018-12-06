0d1d1e12850a620430cec36d190de12da6a26afc
takesi <nanasetakesi@yahoo.co.jp>
2018-12-06 17:15:51 +0800

    nacos-config配置中心

---
+++ spring-cloud-discovery/src/main/java/com/pass/cloud/discovery/SpringCloudDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.pass.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author takesi
+ */
+@SpringBootApplication
+public class SpringCloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDiscoveryApplication.class, args);
+    }
+}
