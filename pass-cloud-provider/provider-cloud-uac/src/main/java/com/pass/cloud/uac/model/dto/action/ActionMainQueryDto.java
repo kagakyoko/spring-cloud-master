2306ff3a6f441146ce4cd158c8320f8878e3e237
takesi <nanasetakesi@yahoo.co.jp>
2018-12-12 11:30:42 +0800

    分页查询角色信息、删除 批量删除角色信息 接口

--- pass-cloud-provider/provider-cloud-uac/src/main/java/com/pass/cloud/uac/model/dto/action/ActionMainQueryDto.java
+++ pass-cloud-provider/provider-cloud-uac/src/main/java/com/pass/cloud/uac/model/dto/action/ActionMainQueryDto.java
@@ -3,7 +3,9 @@ package com.pass.cloud.uac.model.dto.action;
 import java.util.List;
 
 import com.pass.cloud.base.dto.BaseQuery;
+
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -19,26 +21,31 @@ public class ActionMainQueryDto extends BaseQuery {
     /**
      * 资源路径
      */
+    @ApiModelProperty(name = "资源路径")
     private String url;
 
     /**
      * 权限名称
      */
+    @ApiModelProperty(name = "权限名称")
     private String actionName;
 
     /**
      * 权限编码
      */
+    @ApiModelProperty(name = "权限编码")
     private String actionCode;
 
     /**
      * 状态
      */
+    @ApiModelProperty(name = "状态")
     private String status;
 
     /**
      * 菜单ID
      */
+    @ApiModelProperty(name = "菜单ID")
     private List<Long> menuIdList;
 
 }
---
+++ pass-cloud-provider/provider-cloud-uac/src/main/java/com/pass/cloud/uac/web/admin/UacActionRestController.java
@@ -0,0 +1,76 @@
+package com.pass.cloud.uac.web.admin;
+
+import java.util.List;
+
+import com.github.pagehelper.PageInfo;
+import com.pass.cloud.core.support.BaseController;
+import com.pass.cloud.uac.model.dto.action.ActionMainQueryDto;
+import com.pass.cloud.uac.model.vo.ActionVo;
+import com.pass.cloud.uac.service.UacActionService;
+import com.pass.cloud.wrapper.WrapMapper;
+import com.pass.cloud.wrapper.Wrapper;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author takesi
+ */
+@RestController
+@RequestMapping(value = "/action")
+@Api(value = "Web - UacActionRestController", tags = "UacActionRestController", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UacActionRestController extends BaseController {
+
+    @Autowired
+    private UacActionService uacActionService;
+
+    /**
+     * 分页查询角色信息.
+     *
+     * @param action the action
+     * @return the wrapper
+     */
+    @ApiOperation(httpMethod = "GET", value = "查询角色列表")
+    @GetMapping(value = "/queryListWithPage", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PageInfo<ActionVo> queryUacActionListWithPage(@ApiParam(name = "action", value = "角色信息") ActionMainQueryDto action) {
+        PageInfo<ActionVo> pageInfo = uacActionService.queryActionListWithPage(action);
+        return pageInfo;
+    }
+
+    /**
+     * 删除角色信息.
+     *
+     * @param id the id
+     * @return the wrapper
+     */
+    @ApiOperation(httpMethod = "DELETE", value = "删除角色")
+    @DeleteMapping(value = "/deleteActionById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Wrapper deleteUacActionById(@ApiParam(name = "id", value = "角色id", required = true) @PathVariable Long id) {
+        int result = uacActionService.deleteActionById(id);
+        return super.handleResult(result);
+    }
+
+    /**
+     * 批量删除角色.
+     *
+     * @param deleteIdList the delete id list
+     * @return the wrapper
+     */
+
+    @ApiOperation(httpMethod = "DELETE", value = "批量删除角色")
+    @DeleteMapping(value = "/batchDeleteByIdList", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Wrapper batchDeleteByIdList(@ApiParam(name = "deleteIdList", value = "角色Id") @RequestBody List<Long> deleteIdList) {
+        uacActionService.batchDeleteByIdList(deleteIdList);
+        return WrapMapper.ok();
+    }
+
+}
