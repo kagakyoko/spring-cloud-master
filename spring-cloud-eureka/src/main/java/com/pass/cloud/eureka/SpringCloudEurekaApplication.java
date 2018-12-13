adc4cd04362a17d6c8b62eea4187116389e9ebda
takesi <nanasetakesi@yahoo.co.jp>
2018-12-13 16:45:25 +0800

    创建注册中心，配置docker插件

---
+++ spring-cloud-eureka/src/main/java/com/pass/cloud/eureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,19 @@
+package com.pass.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author takesi
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+
+}
+
